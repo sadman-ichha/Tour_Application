0a56d0b1d777c326377e23441fef7b8b02b72d30
sadman_ichha <sadmanichha1@gmail.com>
2022-09-17 12:43:34 +0600

    commit

--- lib/ui/views/bottom_nav_controller/pages/nav_home.dart
+++ lib/ui/views/bottom_nav_controller/pages/nav_home.dart
@@ -5,6 +5,7 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:get/get.dart';
 
 import 'package:shelter/ui/route/route.dart';
+import 'package:shelter/ui/widgets/nav_home_categories.dart';
 
 import '../search_screen.dart';
 
@@ -21,77 +22,103 @@ class NavHome extends StatelessWidget {
   Widget build(BuildContext context) {
     return SafeArea(
       child: Scaffold(
-        body: Padding(
-          padding: EdgeInsets.only(top: 4.0),
-          child: Column(
-            children: [
-              AspectRatio(
-                aspectRatio: 3.5,
-                child: CarouselSlider(
-                  options: CarouselOptions(
-                    height: 155.0.h,
-                    onPageChanged: (currentIndex, customPageChangedReason) {
-                      _currentIndex.value = currentIndex;
-                    },
-                  ),
-                  items: _carouselImages.map((images) {
-                    return Container(
-                      margin: EdgeInsets.only(left: 10.0),
-                      decoration: BoxDecoration(
-                        color: Colors.blue,
-                        image: DecorationImage(
-                          image: AssetImage(images),
-                          fit: BoxFit.cover,
-                        ),
-                      ),
-                    );
-                  }).toList(),
+        body: Column(
+          children: [
+            AspectRatio(
+              aspectRatio: 3.5,
+              child: CarouselSlider(
+                options: CarouselOptions(
+                  height: 155.0.h,
+                  onPageChanged: (currentIndex, customPageChangedReason) {
+                    _currentIndex.value = currentIndex;
+                  },
                 ),
-              ),
-              SizedBox(height: 5.h),
-              Obx(
-                () => DotsIndicator(
-                  dotsCount: _carouselImages.length,
-                  position: _currentIndex.value.toDouble(),
-                ),
-              ),
-              Padding(
-                padding: EdgeInsets.only(
-                    left: 53.w, right: 52.w, top: 10.h, bottom: 10.h),
-                child: InkWell(
-                  onTap: () => Get.toNamed(search),
-                  child: Container(
-                    height: 45.0.h,
+                items: _carouselImages.map((images) {
+                  return Container(
+                    margin: EdgeInsets.only(left: 10.0),
                     decoration: BoxDecoration(
-                      color: Colors.white38,
-                      borderRadius: BorderRadius.all(
-                        Radius.circular(6.r),
+                      color: Colors.blue,
+                      image: DecorationImage(
+                        image: AssetImage(images),
+                        fit: BoxFit.cover,
                       ),
                     ),
-                    child: Padding(
-                      padding: EdgeInsets.only(left: 20.w),
-                      child: Row(
-                        children: [
-                          Icon(
-                            Icons.search_outlined,
-                            size: 27.0.w,
-                          ),
-                          SizedBox(width: 15.0.w),
-                          Text(
-                            'Search for your next tour',
-                            style: TextStyle(
-                                fontSize: 15.0.sp, fontWeight: FontWeight.w400),
-                          )
-                        ],
-                      ),
+                  );
+                }).toList(),
+              ),
+            ),
+            SizedBox(height: 5.h),
+            Obx(
+              () => DotsIndicator(
+                dotsCount: _carouselImages.length,
+                position: _currentIndex.value.toDouble(),
+              ),
+            ),
+            Padding(
+              padding: EdgeInsets.only(
+                  left: 53.w, right: 52.w, top: 10.h, bottom: 10.h),
+              child: InkWell(
+                onTap: () => Get.toNamed(search),
+                child: Container(
+                  height: 45.0.h,
+                  decoration: BoxDecoration(
+                    color: Colors.white38,
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(6.r),
+                    ),
+                  ),
+                  child: Padding(
+                    padding: EdgeInsets.only(left: 20.w),
+                    child: Row(
+                      children: [
+                        Icon(
+                          Icons.search_outlined,
+                          size: 27.0.w,
+                        ),
+                        SizedBox(width: 15.0.w),
+                        Text(
+                          'Search for your next tour',
+                          style: TextStyle(
+                              fontSize: 15.0.sp, fontWeight: FontWeight.w400),
+                        ),
+                      ],
                     ),
                   ),
                 ),
-              )
-            ],
-          ),
+              ),
+            ),
+            SizedBox(height: 61.0.h),
+            navHomeCategories('For You', () {}),
+            SizedBox(height: 21.0.h),
+            forYou(),
+            
+          ],
         ),
       ),
     );
   }
+}
+
+Widget forYou() {
+  return Container(
+    height: 180.0.h,
+    child: ListView.builder(
+      scrollDirection: Axis.horizontal,
+        itemCount: 12,
+        itemBuilder: (_, index) {
+          return Padding(
+            padding:  EdgeInsets.only(right: 12.w),
+            child: Container(
+              width: 100.w,
+                height: 180.h,
+              decoration: BoxDecoration(
+                color: Colors.blue,
+                borderRadius: BorderRadius.all(
+                    Radius.circular(7.r),
+              ),
+              ),
+            ),
+          );
+        }),
+  );
 }
---
+++ lib/ui/widgets/nav_home_categories.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:shelter/const/app_colors.dart';
+
+Widget navHomeCategories(String categoryName, onClick) {
+  return Padding(
+    padding: EdgeInsets.only(left: 37.0.w, right: 40.0.w),
+    child: Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: [
+        Text(
+          categoryName,
+          style: TextStyle(fontSize: 16.0.sp, fontWeight: FontWeight.w500),
+        ),
+        InkWell(
+          onTap: () {},
+          child: Text(
+            "See All",
+            style: TextStyle(
+                fontSize: 14.0.sp,
+                fontWeight: FontWeight.w500,
+                color: AppColors.violetColor),
+          ),
+        ),
+      ],
+    ),
+  );
+}
