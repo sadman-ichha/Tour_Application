97d3c5c5fb163c30008b4635560e1278cfa714c4
sadman_ichha <sadmanichha1@gmail.com>
2022-09-19 11:58:06 +0600

    commit

--- lib/ui/views/bottom_nav_controller/pages/nav_add_last_step.dart
+++ lib/ui/views/bottom_nav_controller/pages/nav_add_last_step.dart
@@ -1,10 +1,25 @@
+import 'dart:io';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:image_picker/image_picker.dart';
 import 'package:shelter/ui/widgets/custom_text_field.dart';
+import 'package:shelter/ui/widgets/violet_button.dart';
 
-class NavAddLastStep extends StatelessWidget {
-  const NavAddLastStep({Key? key}) : super(key: key);
+class NavAddLastStep extends StatefulWidget {
+  @override
+  State<NavAddLastStep> createState() => _NavAddLastStepState();
+}
+
+class _NavAddLastStepState extends State<NavAddLastStep> {
+  final ImagePicker _picker = ImagePicker();
+
+  List<XFile>? images;
+
+  Future multipleImagePicker() async {
+    images = await _picker.pickMultiImage();
+    setState(() {});
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -13,37 +28,64 @@ class NavAddLastStep extends StatelessWidget {
         backgroundColor: Colors.white,
         body: Padding(
           padding: EdgeInsets.only(left: 25.0.w, right: 25.0.w, top: 10.0.h),
-          child: Column(
-            children: [
-              Text("Fill out the form and start the journey",
-                  style: TextStyle(
-                    fontSize: 24.0.sp,
-                    fontWeight: FontWeight.w300,
-                  )),
-              SizedBox(height: 20.h),
-              customTextField("Phone Number"),
-              customTextField('Destination Date & Time'),
-              Align(
-                alignment: AlignmentDirectional.topStart,
-                child: Text(
-                  'Choose Images',
-                  style: TextStyle(
-                    fontWeight: FontWeight.w500,
-                    fontSize: 18.sp,
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                Text("Fill out the form and start the journey",
+                    style: TextStyle(
+                      fontSize: 24.0.sp,
+                      fontWeight: FontWeight.w300,
+                    )),
+                SizedBox(height: 20.h),
+                customTextField("Phone Number"),
+                customTextField('Destination Date & Time'),
+                Align(
+                  alignment: AlignmentDirectional.topStart,
+                  child: Text(
+                    'Choose Images',
+                    style: TextStyle(
+                      fontWeight: FontWeight.w500,
+                      fontSize: 18.sp,
+                    ),
                   ),
                 ),
-              ),
-              Container(
-                height: 100.h,
-                decoration: BoxDecoration(
-                  color: Color(0xFFE9EBED),
-                  borderRadius: BorderRadius.all(
-                    Radius.circular(7.r),
+                Container(
+                  height: 152.h,
+                  width: 375.0.w,
+                  decoration: BoxDecoration(
+                    color: Color(0xFFE9EBED),
+                    borderRadius: BorderRadius.all(
+                      Radius.circular(7.r),
+                    ),
                   ),
+                  child: IconButton(
+                      onPressed: () => multipleImagePicker(),
+                      icon: Icon(Icons.add_a_photo)),
                 ),
-                child: ,
-              ),
-            ],
+                Container(
+                  height: 200.0.h,
+                  child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: images?.length,
+                      itemBuilder: (_, index) {
+                        return Padding(
+                          padding: EdgeInsets.only(right: 10.w),
+                          child: Container(
+                              width: 150.0.w,
+                              
+                              child: images?.length == null
+                                  ? null
+                                  : Image.file(
+                                      File(images![index].path),
+                                      fit: BoxFit.cover,
+                                    )),
+                        );
+                      }),
+                ),
+                SizedBox(height: 80.0.h),
+                VioletButton("Upload", () {}),
+              ],
+            ),
           ),
         ),
       ),
--- lib/ui/views/bottom_nav_controller/pages/nav_favorite.dart
+++ lib/ui/views/bottom_nav_controller/pages/nav_favorite.dart
@@ -1,10 +1,86 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
 
 class NavFavorite extends StatelessWidget {
-  const NavFavorite({Key? key}) : super(key: key);
+  // RxBool _isCheckbox = false.obs;
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      backgroundColor: Colors.white,
+      body: Padding(
+        padding: EdgeInsets.only(left: 30.0.w, right: 30.0.w),
+        child: ListView.builder(
+            itemCount: 12,
+            itemBuilder: (_, index) {
+              return Card(
+                elevation: 3,
+                child: Stack(
+                  children: [
+                    Row(
+                      children: [
+                        Expanded(
+                          flex: 1,
+                          child: Container(
+                            height: 110.0.h,
+                            child: Image.asset(
+                              "assets/images/debtakhum.jpeg",
+                              fit: BoxFit.cover,
+                            ),
+                          ),
+                        ),
+                        SizedBox(width: 31.0.w),
+                        Expanded(
+                            flex: 1,
+                            child: navFavText("Title", "Description", "Cost")),
+                        // Obx(
+                        //   () => Checkbox(
+                        //       value: _isCheckbox.value,
+                        //       onChanged: (bool? values) {
+                        //         _isCheckbox.value = values!;
+                        //       }),
+                        // ),
+
+                        Positioned(
+                          child: CircleAvatar(
+                            backgroundColor: Colors.white12,
+                            child: IconButton(
+                              onPressed: () {},
+                              icon: Icon(Icons.close_outlined),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              );
+            }),
+      ),
+    );
   }
+}
+
+Widget navFavText(String title, String description, cost) {
+  return Column(
+    crossAxisAlignment: CrossAxisAlignment.start,
+    children: [
+      Text(
+        title,
+        style: TextStyle(fontSize: 17.0.sp, fontWeight: FontWeight.w500),
+      ),
+      Text(
+        description,
+        style: TextStyle(fontSize: 17.0.sp, fontWeight: FontWeight.w500),
+      ),
+      Text(
+        cost,
+        style: TextStyle(fontSize: 17.0.sp, fontWeight: FontWeight.w600),
+      ),
+    ],
+  );
 }
