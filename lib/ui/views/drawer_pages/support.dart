fe1b33b2e3c1352f6c4a380077f151d740a1c67f
sadman_ichha <sadmanichha1@gmail.com>
2022-09-19 12:52:21 +0600

    commit

--- lib/ui/views/drawer_pages/support.dart
+++ lib/ui/views/drawer_pages/support.dart
@@ -14,15 +14,22 @@ class Support extends StatelessWidget {
         ),),
       
      ),
-     body: Column(
-      children: [
-        Text('If you have any problems , please contact us . We are at your service all the time.',style: TextStyle(
-          fontSize: 20.0.sp, fontWeight: FontWeight.w300,
-        ),),
-        supportField("Phone Number", "01749784788"),
-        supportField("Email Address", "sadmanichha@gmail.com"),
-        supportField("Facebook", "https://www.facebook.com/sadmanichha/"),
-      ],
+     body: Padding(
+       padding:  EdgeInsets.only(left: 25.0.w,right: 20.0.w,top: 20.0.h),
+       child: Column(
+        children: [
+          Text('If you have any problems , please contact us . We are at your service all the time.',style: TextStyle(
+            fontSize: 20.0.sp, fontWeight: FontWeight.w300,
+          ),),
+          SizedBox(height: 20.0.h),
+          supportField("Phone Number", "01749784788" ),
+          SizedBox(height: 26.0.h),
+          supportField("Email Address", "sadmanichha@gmail.com"),
+          SizedBox(height: 26.0.h),
+          supportField("Facebook", "https://www.facebook.com/sadmanichha/",),
+          SizedBox(height: 26.0.h),
+        ],
+       ),
      ),
     );
   }
--- lib/ui/widgets/support_field.dart
+++ lib/ui/widgets/support_field.dart
@@ -1,20 +1,34 @@
+// ignore_for_file: prefer_const_constructors
+
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 
-
-Widget supportField(String title,value) {
-
-TextEditingController _supportDataController = TextEditingController(text: value);
+Widget supportField(String title, value) {
+  TextEditingController _supportDataController =
+      TextEditingController(text: value);
 
   return Column(
     children: [
-      Text(title,style: TextStyle(
+      Text(
+        title,
+        style: TextStyle(
           fontSize: 20.sp,
-        ),),
-        TextField(
-          controller: _supportDataController,
-          readOnly: true,
         ),
+      ),
+      TextField(
+        controller: _supportDataController,
+        readOnly: true,
+        style: TextStyle(fontSize: 20.0.sp, fontWeight: FontWeight.w500),
+        decoration: InputDecoration(
+          enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(
+            width: 1.8.w,
+            color: Colors.green,
+          )),
+        ),
+      ),
     ],
   );
 }
