6c6f89f5dff9afcd384eb6c047d90ef3740311f1
sadman_ichha <sadmanichha1@gmail.com>
2022-09-19 14:49:41 +0600

    commit

--- lib/ui/views/drawer_pages/settings.dart
+++ lib/ui/views/drawer_pages/settings.dart
@@ -1,12 +1,97 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:get/get.dart';
+import 'package:shelter/ui/widgets/drawer_items.dart';
 
 class Setting extends StatelessWidget {
-  // const Settings({Key? key}) : super(key: key);
+  RxBool _darkMode = false.obs;
+
+  Future _logOut(context) async {
+    return showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+              title: Text("Are you sure to logout?"),
+              content: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceAround,
+                children: [
+                  ElevatedButton(
+                      onPressed: () => Get.back(), child: Text("No")),
+                  ElevatedButton(onPressed: () {}, child: Text("Yes")),
+                ],
+              ));
+        });
+  }
+
+  Future _changeLanguage(context) async {
+    return showDialog(
+        context: context,
+        builder: (_) {
+          return AlertDialog(
+            title: Text("Select your language!"),
+            content: Container(
+              height: 150.h,
+              child: Column(
+                children: [
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("Bangla",
+                    
+                    
+                    )),
+                  TextButton(
+                    onPressed: () {},
+                    child: Text("English"),
+                  ),
+                ],
+              ),
+            ),
+          );
+        });
+  }
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
+      appBar: AppBar(
+        backgroundColor: Colors.white,
+        elevation: 0,
+        title: Text("Settings",
+            style: TextStyle(
+              fontSize: 24.0.sp,
+              fontWeight: FontWeight.w400,
+            )),
+      ),
+      body: Padding(
+        padding: EdgeInsets.only(left: 33.0.w, right: 33.0.w, top: 30.0.h),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Text(
+                  "Dark Mode",
+                  style:
+                      TextStyle(fontSize: 20.sp, fontWeight: FontWeight.w400),
+                ),
+                Obx(
+                  () => Switch(
+                      value: _darkMode.value,
+                      onChanged: (bool values) {
+                        _darkMode.value = values;
+                      }),
+                )
+              ],
+            ),
+            drawerItems("Profile", () {}),
+            SizedBox(height: 11.0.h),
+            drawerItems("Languages", () =>_changeLanguage(context)),
+            SizedBox(height: 11.0.h),
+            drawerItems("LogOut", () => _logOut(context)),
+          ],
+        ),
+      ),
     );
   }
 }
--- lib/ui/views/drawer_pages/support.dart
+++ lib/ui/views/drawer_pages/support.dart
@@ -7,30 +7,32 @@ class Support extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-     appBar: AppBar(
-        title: Text("Support",style: TextStyle(
-          fontSize: 24.0.sp, fontWeight: FontWeight.w400,
-        ),),
-      
-     ),
-     body: Padding(
-       padding:  EdgeInsets.only(left: 25.0.w,right: 20.0.w,top: 20.0.h),
-       child: Column(
-        children: [
-          Text('If you have any problems , please contact us . We are at your service all the time.',style: TextStyle(
-            fontSize: 20.0.sp, fontWeight: FontWeight.w300,
+    return SafeArea(
+      child: Scaffold(
+       appBar: AppBar(
+          title: Text("Support",style: TextStyle(
+            fontSize: 24.0.sp, fontWeight: FontWeight.w400,
           ),),
-          SizedBox(height: 20.0.h),
-          supportField("Phone Number", "01749784788" ),
-          SizedBox(height: 26.0.h),
-          supportField("Email Address", "sadmanichha@gmail.com"),
-          SizedBox(height: 26.0.h),
-          supportField("Facebook", "https://www.facebook.com/sadmanichha/",),
-          SizedBox(height: 26.0.h),
-        ],
+        
        ),
-     ),
+       body: Padding(
+         padding:  EdgeInsets.only(left: 25.0.w,right: 20.0.w,top: 20.0.h),
+         child: Column(
+          children: [
+            Text('If you have any problems , please contact us . We are at your service all the time.',style: TextStyle(
+              fontSize: 20.0.sp, fontWeight: FontWeight.w300,
+            ),),
+            SizedBox(height: 20.0.h),
+            supportField("Phone Number", "01749784788" ),
+            SizedBox(height: 26.0.h),
+            supportField("Email Address", "sadmanichha@gmail.com"),
+            SizedBox(height: 26.0.h),
+            supportField("Facebook", "https://www.facebook.com/sadmanichha/",),
+            SizedBox(height: 26.0.h),
+          ],
+         ),
+       ),
+      ),
     );
   }
 }
