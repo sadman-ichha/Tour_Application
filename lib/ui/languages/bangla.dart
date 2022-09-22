9e05cdcbd9c20d1758a7e54f8d71fdc876ee8c7e
sadman_ichha <sadmanichha1@gmail.com>
2022-09-22 11:04:45 +0600

    commit

---
+++ lib/ui/languages/bangla.dart
@@ -0,0 +1,7 @@
+Map <String, String> ban = {
+   "appName": "শেল্টার",
+  "darkMode": "ডার্ক মোড",
+  "profile": "প্রোফাইল",
+  "language": "ভাষা",
+  "logOut": "লগআউট",
+};
---
+++ lib/ui/languages/english.dart
@@ -0,0 +1,7 @@
+Map<String, String> eng = {
+  "appName": "Shelter",
+  "darkMode": "Dark Mode",
+  "profile": "Profile",
+  "language": "Languages",
+  "logOut": "LogOut",
+};
